8285333c7b1e69af1bda5848bac9b26fe5bb8e97
kranti.baral <baralkranti4@gmail.com>
2022-04-01 20:30:57 +0545

    Updated

--- add income/addIncome.php
+++ add income/addIncome.php
@@ -11,4 +11,37 @@ $response = array(
 );
 
 echo json_encode($response);
+require 'db.php';
+
+switch ($method){
+    case 'GET':
+     $sql = "SELECT * FROM addincome";
+     $result = mysqli_query($conn, $sql);
+     $tasks = array();
+     if(mysqli_num_rows($result)>0){
+         while($row = mysqli_fetch_assoc($result)){
+             array_push($tasks, $row);
+         }
+
+         $response = array(
+             'status' => true,
+             'msg' => "Fetched Sucessfully",
+             'data' => $tasks,
+         );
+         echo json_encode($response);
+
+    
+        }else{
+         $response = array(
+             'status' => false,
+             'msg' => "No Transactions Found",
+         );
+         echo json_encode($response);
+     }
+     break; 
+
+     case 'POST':
+        $dat
+
+}
 ?>
